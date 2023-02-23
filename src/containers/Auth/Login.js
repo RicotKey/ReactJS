df5d2f2ee45e01ca83ae1fd9b7c2aa08bbc85fe2
RicotKey <keynguyen.121@gmail.com>
2023-02-23 21:42:20 +0700

    Build Form Login && check event onChange onClick && know about STATE and setState

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -12,18 +12,75 @@ import { FormattedMessage } from 'react-intl';
 class Login extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            email: '',
+            password: '',  
+            isshowpassword: false,
+        }
+    }
+    handleOnChangeEmail = (event)=>{
+        this.setState({
+            email: event.target.value,
+        })
     }
 
+    handleOnChangePassword = (event)=>{
+        this.setState({
+            password: event.target.value,
+        })
+    }
+    handleLogin=()=>{
+        console.log('all state', this.state)
+    }
+    handleShowHidePassword=(event)=>{
+        this.setState({
+            isshowpassword: !this.state.isshowpassword
+        })
+    }
     render() {
-        
-
         return (
            <div className='login-background'>
-            <div className='login-container'>
-                <div className='login-content'>
-
+                <div className='login-container'>
+                    <div className='login-content row'>
+                        <div className='col-12 login-text'>Login</div>
+                        <div className='col-12 form-group login-input'>
+                            <label>Email</label>
+                            <input type='email' 
+                                className='form-control' 
+                                placeholder='Enter your email' 
+                                value={this.state.email}
+                                onChange = {(event)=>this.handleOnChangeEmail(event)}
+                                name='email'>
+                            </input>
+                        </div>
+                        <div className='col-12 form-group login-input'>
+                            <label>Password</label>
+                            <div className='custom-ipnut-password'>
+                                <input type={this.state.isshowpassword?'text':'password'}
+                                    className='form-control' 
+                                    placeholder='Enter your password' 
+                                    value={this.state.password}
+                                    onChange = {(event)=>this.handleOnChangePassword(event)}
+                                    name='password'>
+                                    </input>
+                                    <span onClick={(event)=>this.handleShowHidePassword(event)}> <i className={this.state.isshowpassword?'fas fa-eye':'fas fa-eye-slash'}></i></span>
+                                   
+                          </div> 
+                        </div>
+                        <div className='col-12'>
+                            <button className='btn-login'
+                            onClick={()=> this.handleLogin()}>Login</button>
+                        </div>
+                        <div className='col-12'>
+                            <span className='fogot-password'>Forgot your password ?</span>
+                        </div>
+                        <div className='col-12 text-center mt-3'>Or login with: </div>
+                        <div className='col-12 social-login'>
+                            <i className="fab fa-google google"></i>
+                            <i className="fab fa-facebook-f facebook"></i>
+                        </div>
+                    </div>
                 </div>
-            </div>
            </div>
         )
     }
