3ed92a00f78f6c49cdcdbd1a01bc3ac047abe229
RicotKey <keynguyen.121@gmail.com>
2023-03-22 12:10:10 +0700

    build homepage almost done(medicalfacility,outstandingdoctor, handbook)+ optimal css homepage

--- src/containers/HomePage/Section/MedicalFacility.js
+++ src/containers/HomePage/Section/MedicalFacility.js
@@ -1,46 +1,45 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import './Specialty.scss';
-
+import './MedicalFacility.scss'
 import {FormattedMessage} from 'react-intl'
 import Slider from "react-slick";
+class MedicalFacility extends Component {
 
-class Specialty extends Component {
-
-    
     render() {
+
         return (
-           <div className='section-share section-specialty'>
+            
+            <div className='section-share section-medical-Facility'>
             <div className='section-container'>
                 <div className='section-header'>
-                    <span className='title-section'>Chuyên khoa phổ biến</span>
+                    <span className='title-section'>Cơ sở y tế nổi bật</span>
                     <button className='btn-section'>xem thêm</button>
                 </div>
                 <div className='section-body'>
                     <Slider {...this.props.settings}>
                         <div className='section-customize'>
-                            <div className='bg-image section-specialty'></div>
-                            <div>Cơ xương khớp 1</div>
+                            <div className='bg-image section-medical-Facility'></div>
+                            <div>Hệ thống y tế 1</div>
                         </div>
                         <div className='section-customize'> 
-                            <div className='bg-image section-specialty'></div>
-                            <div>Cơ xương khớp 2</div>
+                            <div className='bg-image section-medical-Facility'></div>
+                            <div>Hệ thống y tế 2</div>
                         </div>
                         <div className='section-customize'>
-                            <div className='bg-image section-specialty'></div>
-                            <div>Cơ xương khớp 3</div>
+                            <div className='bg-image section-medical-Facility'></div>
+                            <div>Hệ thống y tế 3</div>
                         </div>
                         <div className='section-customize'>
-                            <div className='bg-image section-specialty'></div>
-                            <div>Cơ xương khớp 4</div>
+                            <div className='bg-image section-medical-Facility'></div>
+                            <div>Hệ thống y tế 4</div>
                         </div>
                         <div className='section-customize'>
-                           <div className='bg-image section-specialty'></div>
-                           <div>Cơ xương khớp 5</div>
+                           <div className='bg-image section-medical-Facility'></div>
+                           <div>Hệ thống y tế 5</div>
                         </div>
                         <div className='section-customize'>
-                            <div className='bg-image section-specialty'></div>
-                            <div>Cơ xương khớp 6</div>
+                            <div className='bg-image section-medical-Facility'></div>
+                            <div>Hệ thống y tế 6</div>
                         </div>
                     </Slider>
                 </div>
@@ -53,15 +52,12 @@ class Specialty extends Component {
 
 const mapStateToProps = state => {
     return {
-        isLoggedIn: state.user.isLoggedIn,
-        language: state.app.language,
-    };
+        isLoggedIn: state.user.isLoggedIn    };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-       
     };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Specialty);
+export default connect(mapStateToProps, mapDispatchToProps)(MedicalFacility);
